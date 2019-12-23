66697498794dc7f6c2458dac89507de19c263598
chiemi627 <chiemi@cs.tsukuba.ac.jp>
2019-12-24 07:16:47 +0900

    post値が渡ってない

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.set('view engine', 'pug')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(bodyParser.json())
 
-var db = new sqlite3.Database('twitter.db')
+var db = new sqlite3.Database('twitter.db');
+var account = 'mob1';
 
 app.get('/', function (req, res, next) {
     var query = "\
@@ -23,8 +24,42 @@ app.get('/', function (req, res, next) {
         res.render('index', {
             results: rows
         })
-    })
+    });
+    next;
+});
+
+app.post('/tweet', function (req, res, next) {
+
+    for (key in req.body) {
+        console.log(key, '=', req.body[key]); // コンソール出力
+    }
+
+    datetime = getCurrentTime();
+    content = req.body['tweet_content'];
+    var query = "INSERT INTO tweet(account,datetime,content) VALUES(?,?,?)";
+    var statement = db.prepare(query);
+    statement.run(account,datetime,content);
+    statement.finalize();
+    
+    res.redirect('/');    
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
+function getCurrentTime() {
+	var now = new Date();
+	var res = "" + now.getFullYear() + "/" + padZero(now.getMonth() + 1) + 
+		"/" + padZero(now.getDate()) + " " + padZero(now.getHours()) + ":" + 
+		padZero(now.getMinutes()) + ":" + padZero(now.getSeconds());
+	return res;
+}
+
+function padZero(num) {
+	var result;
+	if (num < 10) {
+		result = "0" + num;
+	} else {
+		result = "" + num;
+	}
+	return result;
+}
