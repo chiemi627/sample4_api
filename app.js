eb8706ef945547476e069aceb46df75c06eb5a46
Chiemi Watanabe <chiemi@slis.tsukuba.ac.jp>
2019-12-27 14:46:34 +0900

    api作成

--- app.js
+++ app.js
@@ -9,18 +9,35 @@ app.use(urlencodedParser);
 app.use(bodyParser.json());
 
 var db = new sqlite3.Database('twitter.db');
-var account = 'mob1';
 
-app.get('/', function (req, res, next) {
+const allowCrossDomain = function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, access-control-allow-origin, Authorization, access_token'
+    )
+
+    if('OPTIONS' == req.method) {
+        res.sendStatus(200)
+    }
+    else{
+        next()
+    }
+}
+
+app.use(allowCrossDomain)
+
+app.get('/timeline/:account', function (req, res, next) {
     var query = "\
         SELECT t.account, u.name, t.datetime, t.content,\
                (SELECT count(*) FROM favorite WHERE tweet_id=t.id) as fav_count, \
                (SELECT count(*) FROM tweet t2 WHERE retweeted_id=t.id) as ret_count \
         FROM tweet t, user u\
-        WHERE ( t.account = '"+account+"' \
+        WHERE ( t.account = '"+req.params["account"]+"' \
           or  t.account IN (SELECT f.followee_account \
                              FROM follow f \
-                             WHERE f.follower_account = '"+account+"')) \
+                             WHERE f.follower_account = '"+req.params["account"]+"')) \
           and t.account = u.account \
         ORDER BY t.datetime desc; \
         ";
@@ -28,16 +45,16 @@ app.get('/', function (req, res, next) {
     db.all(query, {}, function (err, rows) {
         if (err) {
             console.log("ERROR: " + err.message);
+            res.json();
         }
-        res.render('index', {
-            results: rows
-        })
+        res.json(rows);
     });
     next;
 });
 
 app.post('/tweet', function (req, res, next) {
 
+    account = req.body['account'];
     datetime = getCurrentTime();
     content = req.body['tweet_content'];
     var query = "INSERT INTO tweet(account,datetime,content) VALUES(?,?,?)";
@@ -45,7 +62,11 @@ app.post('/tweet', function (req, res, next) {
     statement.run(account,datetime,content);
     statement.finalize();
     
-    res.redirect('/');    
+    res.json({
+        "account": account,
+        "datetime": datetime,
+        "content": content
+    });
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
